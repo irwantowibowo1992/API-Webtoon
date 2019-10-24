24eb37ba6272100bb8494ca22734854d85a890ad
irwantowibowo1992 <irwantowibowo1992@hotmail.com>
2019-10-24 02:05:39 -0700

    add dotenv

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 require('express-group-routes')
 
 const app = express()
-const port = Number(process.env.PORT || 3000)
+const port = process.env.PORT || 3000
 
 
 app.use(bodyParser.json())
